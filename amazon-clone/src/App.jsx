6587c5f2e0de89eeadc31a9b31a70c2bb2a255af
Mesele-Worku <meselew2112@gmail.com>
2025-02-17 02:37:36 +0300

    Carousel Added

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import CarouselEffect from "./Components/Carousel/CarouselEffect";
 import Header from "./Components/Header/Header";
 function App() {
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   );
 }
